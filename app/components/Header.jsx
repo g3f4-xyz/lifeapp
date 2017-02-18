2538dc1d4d04439f0396614a1bfbaa3ece0b214b
G3F4 <kamil.grabek@gmail.com>
2017-02-18 18:17:12 +0100

    [FIX][] Poprawka kompatybiloności do heroku i innych

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -8,7 +8,7 @@ export default class Header extends React.Component {
   render() {
     return (
       <div>
-        <h1 className="header">{this.props.header}</h1>
+        {/* <h1 className="header">{this.props.header}</h1> */}
       </div>
     );
   }
