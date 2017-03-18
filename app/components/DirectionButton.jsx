763e170fbe1bdaef8b827ab071d138f84410aae2
G3F4 <kamil.grabek@gmail.com>
2017-03-18 23:34:05 +0100

    [FRAMEWORK][#6] Wyniesienie logiki związanej z przyciskami do poruszania do nowego kontenera. Usunięcie z App size ze stanu. Przenisienie do Grid'a domyślnego rozmiaru siatki.

--- app/components/DirectionButton.jsx
+++ app/components/DirectionButton.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import FlatButton from 'material-ui/FlatButton';
 
-
 const styles = {
   right: {
     position: 'absolute',
--- app/containers/Grid.jsx
+++ app/containers/Grid.jsx
@@ -1,27 +1,35 @@
 import React from 'react';
 import Module from './Module';
+import DirectionsButtons from './DirectionsButtons';
 import shallowequal from 'shallowequal';
+
+const DEFAULT_SIZE = {
+  columns: 3,
+  rows: 3,
+};
 
 export default class Grid extends React.Component {
   static propTypes = {
     modules: React.PropTypes.array,
     size: React.PropTypes.object,
     viewPortOffset: React.PropTypes.object,
+    onDirection: React.PropTypes.func,
   };
 
   render() {
-    const { size, modules, viewPortOffset } = this.props;
+    const { size = DEFAULT_SIZE, modules, viewPortOffset, onDirection } = this.props;
     const { columns, rows } = size;
 
     return (
       <div>
+        <DirectionsButtons gridSize={size} onDirectionClick={onDirection} viewPortOffset={viewPortOffset} />
         {Array.from({ length: rows }, (_, index) => index).map(rowOffset => (
           <div key={rowOffset}>
             {Array.from({length: columns}, (_, index) => index).map(columnOffset => {
               const module = modules.find((module) => module.offset.column === columnOffset && module.offset.row === rowOffset);
 
               return module && shallowequal(viewPortOffset, module.offset) ? (
-                <Module style={{display: 'inline-flex'}} key={columnOffset}>{module.node}</Module>
+                <Module key={columnOffset}>{module.node}</Module>
               ) : null;
             })}
           </div>
