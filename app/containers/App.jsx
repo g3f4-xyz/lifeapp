f8179af7bb77c29ef4df56f34a0952645796270b
G3F4 <kamil.grabek@gmail.com>
2017-06-29 18:57:14 +0200

    [FRAMEWORK][#68] Grid powinnien utrzymywać moduły przez cały czas w drzewie DOM + zmiana w przekazywaniu modułów do Grid

--- app/containers/App.jsx
+++ app/containers/App.jsx
@@ -11,6 +11,81 @@ import Templates from '../modules/Templates';
 import Settings from '../modules/Settings';
 import Grid from './Grid';
 
+const modules = [
+  {
+    id: 'Home',
+    Component: Home,
+    offset: {
+      column: 1,
+      row: 1,
+    },
+  },
+  {
+    id: 'Enter',
+    Component: Enter,
+    offset: {
+      column: 1,
+      row: 0,
+    },
+  },
+  {
+    id: 'TaskCreate',
+    Component: TaskCreate,
+    offset: {
+      column: 0,
+      row: 0,
+    },
+  },
+  {
+    id: 'TaskEdit',
+    Component: TaskEdit,
+    offset: {
+      column: 2,
+      row: 0,
+    },
+  },
+  {
+    id: 'TaskDetails',
+    Component: TaskDetails,
+    offset: {
+      column: 2,
+      row: 1,
+    },
+  },
+  {
+    id: 'Templates',
+    Component: Templates,
+    offset: {
+      column: 0,
+      row: 1,
+    },
+  },
+  {
+    id: 'Settings',
+    Component: Settings,
+    offset: {
+      column: 1,
+      row: 2,
+    },
+  },
+  {
+    id: 'AttachmentPreview',
+    Component: AttachmentPreview,
+    offset: {
+      column: 2,
+      row: 2,
+    },
+  },
+  {
+    id: 'CustomTemplate',
+    Component: CustomTemplate,
+    offset: {
+      column: 0,
+      row: 2,
+    },
+  },
+];
+
 class App extends React.Component {
   static propTypes = {
     app: React.PropTypes.shape({
@@ -38,94 +113,42 @@ class App extends React.Component {
   onModuleChange = (viewPortOffset) => this.setState({ viewPortOffset });
 
   render() {
-    const modules = [
-      {
-        desc: 'Home module',
-        node: <Home
-          home={this.props.app.home}
-          onDetails={this.onDetails}
-        />,
-        offset: {
-          column: 1,
-          row: 1,
-        },
-      },
-      {
-        desc: 'Enter module',
-        node: <Enter />,
-        offset: {
-          column: 1,
-          row: 0,
-        },
-      },
-      {
-        desc: 'TaskCreate module',
-        node: <TaskCreate />,
-        offset: {
-          column: 0,
-          row: 0,
-        },
-      },
-      {
-        desc: 'TaskEdit module',
-        node: <TaskEdit />,
-        offset: {
-          column: 2,
-          row: 0,
-        },
-      },
-      {
-        desc: 'TaskDetails module',
-        node: <TaskDetails
-          taskDetails={this.props.app.taskDetails}
-          onDetails={this.onDetails}
-        />,
-        offset: {
-          column: 2,
-          row: 1,
-        },
-      },
-      {
-        desc: 'Templates module',
-        node: <Templates />,
-        offset: {
-          column: 0,
-          row: 1,
-        },
-      },
-      {
-        desc: 'Settings module',
-        node: <Settings />,
-        offset: {
-          column: 1,
-          row: 2,
-        },
-      },
-      {
-        desc: 'AttachmentPreview module',
-        node: <AttachmentPreview />,
-        offset: {
-          column: 2,
-          row: 2,
-        },
-      },
-      {
-        desc: 'CustomTemplate module',
-        node: <CustomTemplate />,
-        offset: {
-          column: 0,
-          row: 2,
-        },
-      },
-    ];
 
     return (
       <div>
         <Grid
           modules={modules}
+          handlers={{
+            Home: () => ({
+              home: this.props.app.home,
+              onDetails: this.onDetails,
+            }),
+            TaskDetails: () => ({
+              taskDetails: this.props.app.taskDetails,
+              onDetails: this.onDetails,
+            }),
+          }}
           viewPortOffset={this.state.viewPortOffset}
           onModuleChange={this.onModuleChange}
-        />
+        >
+          <Home
+            id="Home"
+            home={this.props.app.home}
+            onDetails={this.onDetails}
+          />
+          <Enter id="Enter" />
+          <TaskCreate id="TaskCreate" />
+          <TaskEdit id="TaskEdit" />
+          <TaskDetails
+            id="TaskDetails"
+            taskDetails={this.props.app.taskDetails}
+            onDetails={this.onDetails}
+          />
+          <Templates id="Templates" />
+          <Settings id="Settings" />
+          <AttachmentPreview id="AttachmentPreview" />
+          <CustomTemplate id="CustomTemplate" />
+        </Grid>
       </div>
     );
   }
