961ba362dcb3dba75911fe56e8a9ce752aaab67b
G3F4 <kamil.grabek@gmail.com>
2017-11-22 16:44:30 +0100

    [FIX] Zmiana callbackURL dla strategi autoryzacji google

--- server/authentication.js
+++ server/authentication.js
@@ -1,11 +1,11 @@
 const passport = require('passport');
 const { Strategy: GoogleStrategy } = require('passport-google-oauth20');
-const { HOST, PORT } = require('./config');
+const { HOST } = require('./config');
 
 passport.use(new GoogleStrategy({
   clientID: '84842929925-8b9p5173tjtkrttoabdb4ofg670rhfqa.apps.googleusercontent.com', // #TODO to powinno pochodzić z procesu albo konfiguracji
   clientSecret: 'zW2ebcvBaxAZFqMu00OTb9-d', // #TODO jw.
-  callbackURL: `${HOST}:${PORT}/auth/google/logged`,
+  callbackURL: `${HOST}/auth/google/logged`,
 },
 (accessToken, refreshToken, profile, cb) => {
   console.log(['GoogleStatery callback'], accessToken, refreshToken, profile, cb);
