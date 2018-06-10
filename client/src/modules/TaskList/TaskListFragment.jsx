ca1c4f7c6636c531678a4793d2e63dabfc943c2e
Kamil Grabek <kamil.grabek@allegro.pl>
2018-06-10 23:36:35 +0200

    [#101] Dodanie ikony na liście zadań na belce dla statusu i priorytetu wraz z kolorami

--- client/src/modules/TaskList/TaskListFragment.jsx
+++ client/src/modules/TaskList/TaskListFragment.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 import { createFragmentContainer, graphql } from 'react-relay';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import CheckBoxIcon from '@material-ui/icons/CheckBox';
+import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';
+import ErrorIcon from '@material-ui/icons/Error';
+import ReportIcon from '@material-ui/icons/Report';
+import SlideshowIcon from '@material-ui/icons/Slideshow';
+import WarningIcon from '@material-ui/icons/Warning';
+import Tooltip from '@material-ui/core/Tooltip';
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import green from '@material-ui/core/colors/green';
+import grey from '@material-ui/core/colors/grey';
+import red from '@material-ui/core/colors/red';
+import yellow from '@material-ui/core/colors/yellow';
 
 const styles = theme => ({
   actions: {
@@ -19,8 +31,26 @@ const styles = theme => ({
     display: 'block',
   },
   heading: {
-    fontSize: theme.typography.pxToRem(15),
+    flex: 1,
+    textAlign: 'center',
+    fontSize: theme.typography.pxToRem(20),
     fontWeight: theme.typography.fontWeightRegular,
+    marginTop: theme.spacing.unit,
+  },
+  icon: {
+    fontSize: theme.spacing.unit * 5,
+  },
+  green: {
+    color: green[700],
+  },
+  grey: {
+    color: grey[700],
+  },
+  red: {
+    color: red[700],
+  },
+  yellow: {
+    color: yellow[700],
   },
 });
 
@@ -45,22 +75,32 @@ class TaskListFragment extends React.PureComponent {
     return (
       <ExpansionPanel>
         <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+          <div>
+            <Tooltip title={`Priority: ${priority}`}>
+              {{
+                LOW: <ReportIcon className={classnames(classes.grey, classes.icon)} />,
+                NORMAL: <WarningIcon className={classnames(classes.yellow, classes.icon)} />,
+                HIGH: <ErrorIcon className={classnames(classes.red, classes.icon)} />,
+              }[priority]}
+            </Tooltip>
+            <Tooltip title={`Status: ${status}`}>
+              {{
+                TODO: <CheckBoxOutlineBlankIcon className={classnames(classes.grey, classes.icon)} />,
+                IN_PROGRESS: <SlideshowIcon className={classnames(classes.yellow, classes.icon)} />,
+                DONE: <CheckBoxIcon className={classnames(classes.green, classes.icon)} />,
+              }[status]}
+            </Tooltip>
+          </div>
           <Typography className={classes.heading}>{`${title} (${taskType})`}</Typography>
         </ExpansionPanelSummary>
         <ExpansionPanelDetails className={classes.content}>
           <div>
-            PRIORITY: {priority}
-          </div>
-          <div>
-            STATUS: {status}
+            {description}
           </div>
           <div className={classes.actions}>
             <Button onClick={() => onDetails(id)}>Show</Button>
             <Button onClick={() => onEdit(id)}>Edit</Button>
             <Button onClick={() => onDelete(id)}>Delete</Button>
-          </div>
-          <div>
-            {description}
           </div>
         </ExpansionPanelDetails>
       </ExpansionPanel>
