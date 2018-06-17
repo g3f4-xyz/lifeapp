d3b6c3dd76a2bfa366dd85f758ecac5a5cd8824b
Kamil Grabek <kamil.grabek@allegro.pl>
2018-06-17 21:37:48 +0200

    [#110] Ikony na podglądzie zadania

--- client/src/modules/Task/TaskDetails.jsx
+++ client/src/modules/Task/TaskDetails.jsx
@@ -2,17 +2,33 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
-import Create from '@material-ui/icons/Create';
+import BatteryAlertIcon from '@material-ui/icons/BatteryAlert';
+import CreateIcon from '@material-ui/icons/Create';
+import PersonPinCircleIcon from '@material-ui/icons/PersonPinCircle';
+import PriorityHighIcon from '@material-ui/icons/PriorityHigh';
+import NotificationsActiveIcon from '@material-ui/icons/NotificationsActive';
+import EventNoteIcon from '@material-ui/icons/EventNote';
+import TitleIcon from '@material-ui/icons/Title';
+import PersonIcon from '@material-ui/icons/Person';
 import { Label } from '../../components/index';
 
-const styles = {
+const ICONS = {
+  DATE: NotificationsActiveIcon,
+  NOTE: EventNoteIcon,
+  PRIORITY: PriorityHighIcon,
+  TITLE: TitleIcon,
+  STATUS: BatteryAlertIcon,
+  PERSON: PersonIcon,
+  LOCATION: PersonPinCircleIcon,
+};
+
+const styles = theme => ({
+  icon: {
+    margin: theme.spacing.unit,
+  },
   label: {
     padding: 10,
-  },
-  labelContainer: {
-    padding: 10,
     width: 200,
-    textAlign: 'left',
   },
   value: {
     padding: 20,
@@ -25,14 +41,23 @@ const styles = {
     display: 'flex',
     justifyContent: 'space-between',
     margin: 10,
+    alignItems: 'center',
   },
-};
+});
 
 class TaskDetails extends React.Component {
   static propTypes = {
     classes: PropTypes.object,
     data: PropTypes.object,
   };
+
+  renderIcon(fieldId) {
+    const Icon = ICONS[fieldId] || CreateIcon;
+
+    return (
+      <Icon className={this.props.classes.icon} />
+    )
+  }
 
   render() {
     console.log(['TaskDetails:render'], this.props);
@@ -48,10 +73,8 @@ class TaskDetails extends React.Component {
         .map(({ fieldId, label, type, meta: { options }, value }) => (
         <div key={fieldId}>
           <Paper className={classes.row}>
-            <div className={classes.labelContainer}>
-              <Create />
-              <Label class>{label}</Label>
-            </div>
+            {this.renderIcon(fieldId)}
+            <Label className={classes.label} class>{label}</Label>
             <div className={classes.valueContainer}>
               <div className={classes.value}>{value.text || value.number || value.id}</div>
             </div>
